68998f98b46b7c72db1c046501454d237db600ff
jonjharrison <jj2harrison@gmail.com>
2019-07-19 10:04:40 -0700

    Function argument order

--- mediamathclient/lineitem.py
+++ mediamathclient/lineitem.py
@@ -13,21 +13,21 @@ class LineItem(Base):
 
     def get_lineitem_by_id(self, lineitem_id):
         url = self.generate_url('strategies') + "/" + str(lineitem_id)
-        return self.call_mm_api(url, 'GET')
+        return self.call_mm_api('GET', url)
 
     def get_lineitems_by_campaign(self, campaign_id):
         campaign_id = int(campaign_id)
         url = self.generate_url('strategies') + "/limit/campaign={0}/?full=*".format(str(campaign_id))
-        return self.call_mm_api(url, 'GET')
+        return self.call_mm_api('GET', url)
 
     def create_lineitem(self, payload):
         url = self.generate_url('strategies')
-        return self.call_mm_api(url, 'POST', payload)
+        return self.call_mm_api('POST', url, payload)
 
     # updates existing line items
     def update_lineitem(self, payload, lineitem_id):
         url = self.generate_url('strategies') + "/" + str(lineitem_id)
-        return self.call_mm_api(url, 'POST', payload)
+        return self.call_mm_api('POST', url, payload)
 
     def assign_sitelist_to_strategy(self, lineitem_id, sitelist_ids):
         url = self.generate_url('strategies') + "/" + str(lineitem_id) + "/site_lists"
@@ -38,7 +38,7 @@ class LineItem(Base):
             payload[index] = sitelist_id
             payload[assigned] = int(True)
 
-        return self.call_mm_api(url, 'POST', payload)
+        return self.call_mm_api('POST', url, payload)
 
     def remove_sitelist_from_strategy(self, lineitem_id, sitelist_ids):
         url = self.generate_url('strategies') + "/" + str(lineitem_id) + "/site_lists"
@@ -50,7 +50,7 @@ class LineItem(Base):
             payload[index] = sitelist_id
             payload[assigned] = int(False)
 
-        return self.call_mm_api(url, 'POST', payload)
+        return self.call_mm_api('POST', url, payload)
 
     def update_strategy_domain_restrictions(self, lineitem_id, domains):
         url = self.generate_url('strategies') + "/" + str(lineitem_id) + "/domain_restrictions"
@@ -62,7 +62,7 @@ class LineItem(Base):
             payload[index_domain] = domain
             payload[index_restriction] = "INCLUDE"
 
-        return self.call_mm_api(url, 'POST', payload)
+        return self.call_mm_api('POST', url, payload)
 
     def set_deal_targeting_for_strategy(self, lineitem_id, deal_ids):
         url = self.generate_url('strategies') + "/" + str(lineitem_id) + "/deals"
@@ -75,7 +75,7 @@ class LineItem(Base):
         payload["all_pmp"] = 0
         payload["all_exchanges"] = 0
 
-        return self.call_mm_api(url, 'POST', payload)
+        return self.call_mm_api('POST', url, payload)
 
     def set_strategy_exchanges(self, lineitem_id, exchange_ids):
         lineitem_id = int(lineitem_id)
@@ -89,7 +89,7 @@ class LineItem(Base):
         payload["all_pmp"] = 0
         payload["all_exchanges"] = 0
 
-        return self.call_mm_api(url, 'POST', payload)
+        return self.call_mm_api('POST', url, payload)
 
     # TODO: this should be its own class
     def get_deals(self):
@@ -98,7 +98,7 @@ class LineItem(Base):
         url = self.generate_url('deals') + "/?full=*"
         initial_response = requests.get(url, headers=self.headers)
         request_body = self.generate_curl_command('GET', url, self.headers)
-        
+
         if 'errors' in initial_response.json():
             response_json = self.generate_json_response(initial_response.json(), initial_response, request_body)
             return json.dumps(response_json)
