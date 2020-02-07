79cde40f749fb51ed123a91bdd8905e7c37aa63a
Matt Artingstall <martingstall@Matts-MacBook-Air.local>
2020-02-07 04:09:37 -0800

    Add organization_id to advertiser pull

--- mediamathclient/advertiser.py
+++ mediamathclient/advertiser.py
@@ -4,5 +4,7 @@ from mediamathclient.base import Base
 class Advertiser(Base):
 
     def get_all(self):
-        url = self.generate_url("advertisers")
+        url = self.generate_url("advertisers") + "/?owner.organization_id={0}&full=*".format(
+            self.organization_id
+        )
         return self.call_mm_api('GET', url)
